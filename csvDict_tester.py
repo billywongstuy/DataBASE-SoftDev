0cc51911089e88ee8669fb73d97969f08bebed1f
Billy Wong <bwong5@stuy.edu>
2016-10-15 12:27:21 -0400

    basic build?

--- csvDict_tester.py
+++ csvDict_tester.py
@@ -3,6 +3,18 @@ import csv
 fObj = open("peeps.csv") 
 d=csv.DictReader(fObj)
 
-for k in d:
-    print k
+for row in d:
+    insertion = "INSERT INTO students VALUES (\"" + row["name"] + "\", " + row["id"] + ")"
+    print (insertion)
+
+print ("\n\n\n")
+
+fObj2 = open("courses.csv") 
+d2=csv.DictReader(fObj2)
+
+for row in d2:
+    insertion = "INSERT INTO courses VALUES (\"%s\", %s, %s)" % (row["code"], row["id"], row["mark"])
+    print (insertion)
+
+
     
