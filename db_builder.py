caf0cec2e1c958b1fdaf3a7f75bb13292b79729d
Lawrence Liu <lawrence255@yahoo.com>
2016-10-15 20:54:13 -0400

    Added comments and cleaned files.

--- db_builder.py
+++ db_builder.py
@@ -19,15 +19,15 @@ c.execute(q)
 
 
 fObj1 = open("peeps.csv")
-d = csv.DictReader(fObj1)
+d = csv.DictReader(fObj1) 
 for row in d:
-    insertion = "INSERT INTO students VALUES (\"" + row["name"] + "\", " + row["id"] + ")"
+    insertion = "INSERT INTO students VALUES (\"" + row["name"] + "\", " + row["id"] + ")" #Adds each name and ID into the students table
     c.execute(insertion)
 
 fObj2 = open("courses.csv")
 e = csv.DictReader(fObj2)
 for row in e:
-    insertion = "INSERT INTO courses VALUES (\"%s\", %s, %s)" % (row["code"], row["id"], row["mark"])
+    insertion = "INSERT INTO courses VALUES (\"%s\", %s, %s)" % (row["code"], row["id"], row["mark"]) #Adds each course, id, and grade into the courses table
     c.execute(insertion)
 
 
