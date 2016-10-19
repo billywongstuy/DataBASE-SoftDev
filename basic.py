2193882cb6ab6f4cad31012e6988fb837679fdb4
Lawrence Liu <lawrence255@yahoo.com>
2016-10-19 14:30:24 -0400

    one more

--- basic.py
+++ basic.py
@@ -12,10 +12,30 @@ print a #for row in a:
    # print row[0]'''
 
 def grades(id):
-    q = "SELECT mark FROM courses WHERE id=" + str(id)
+    q = "SELECT name FROM students WHERE id=%d" % (id)
+    a = c.execute(q)
+    for row in a:
+        print "Student: " + row[0]
+    q = "SELECT code,mark FROM courses WHERE id=%d" % (id)
     a = c.execute(q) #a should be a dictionary, why is it a cursor???????
-    marks = a
-    for row in marks:
-        print marks[0]
+    for row in a:
+        print row[0] + ": " + str(row[1])
 
+
+def average(id):
+    q = "SELECT name FROM students WHERE id=%d" % (id)
+    a = c.execute(q)
+    for row in a:
+        print "Student: " + row[0]
+    q = "SELECT mark FROM courses WHERE id=%d" % (id)
+    a = c.execute(q) #a should be a dictionary, why is it a cursor???????
+    classNo = 0
+    totalPts = 0
+    for row in a:
+        totalPts += row[0]
+        classNo += 1
+    print "Average: %d" % (totalPts/classNo)
+    
+        
 grades(1)
+average(1)
